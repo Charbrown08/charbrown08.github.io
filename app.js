fc6c73e9cf83203c5852ebd267bd72a9d237c75c
Charbrown08 <carlos.moreno.jimenez@konecta-group.com>
2024-08-30 04:00:25 -0500

    [CHANGE] interactive

--- app.js
+++ app.js
@@ -2,14 +2,27 @@ $(document).ready(function () {
   const username = 'Charbrown08'
   const repoContainer = $('#repos')
 
+  // Array de imágenes personalizadas para cada proyecto
+  const images = [
+    './images/git (2).png',
+    './images/git (1).png',
+    './images/git.png',
+    './images/github.png',
+    './images/git (3).png',
+    './images/git (1).png',
+  ]
+
   fetch(`https://api.github.com/users/${username}/repos`)
     .then((response) => response.json())
     .then((repos) => {
-      repos.slice(0, 6).forEach((repo) => {
+      repos.slice(0, 6).forEach((repo, index) => {
+        // Asigna una imagen del array
+        const image = images[index % images.length]
+
         const repoHTML = `
                 <div class="col-md-4 mb-4" data-aos="flip-left">
                     <div class="card h-100">
-                        <img src="ruta/al/icono" class="card-img-top" alt="Icono del proyecto">
+                        <img src="${image}" class="card-img-top project-img" alt="Imagen del proyecto">
                         <div class="card-body">
                             <h5 class="card-title">${repo.name}</h5>
                             <p class="card-text">${
