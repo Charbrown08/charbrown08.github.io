8b981f5c3dee54b40b50f65c90d88c76f2d58f0f
Charbrown08 <carlos.moreno.jimenez@konecta-group.com>
2024-08-30 03:17:13 -0500

    [CHANGE] interactive

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-  // Usar GitHub API para mostrar proyectos
   const username = 'Charbrown08'
   const repoContainer = $('#repos')
 
@@ -8,8 +7,9 @@ $(document).ready(function () {
     .then((repos) => {
       repos.slice(0, 6).forEach((repo) => {
         const repoHTML = `
-                <div class="col-md-4 mb-4">
+                <div class="col-md-4 mb-4" data-aos="flip-left">
                     <div class="card h-100">
+                        <img src="ruta/al/icono" class="card-img-top" alt="Icono del proyecto">
                         <div class="card-body">
                             <h5 class="card-title">${repo.name}</h5>
                             <p class="card-text">${
@@ -17,7 +17,7 @@ $(document).ready(function () {
                             }</p>
                             <a href="${
                               repo.html_url
-                            }" class="btn btn-primary" target="_blank">Ver en GitHub</a>
+                            }" class="btn btn-warning" target="_blank">Ver en GitHub</a>
                         </div>
                     </div>
                 </div>`
@@ -25,4 +25,16 @@ $(document).ready(function () {
       })
     })
     .catch((error) => console.error('Error al obtener los repos:', error))
+
+  AOS.init({
+    duration: 1200,
+  })
+
+  $('.card').on('mouseover', function () {
+    $(this).css('box-shadow', '0 10px 20px rgba(0, 0, 0, 0.5)')
+  })
+
+  $('.card').on('mouseout', function () {
+    $(this).css('box-shadow', 'none')
+  })
 })
